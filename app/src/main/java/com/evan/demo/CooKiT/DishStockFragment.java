c7d2b090146d2c0232420453a38fdc6d95b4f07a
huangyuan9811 <1296723438huang@gmail.com>
2019-12-11 00:28:29 +0900

    change bottomnavigation

--- app/src/main/java/com/evan/demo/CooKiT/DishStockFragment.java
+++ app/src/main/java/com/evan/demo/CooKiT/DishStockFragment.java
@@ -4,37 +4,53 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
+public class DishStockFragment extends ListFragment {
 
+    private AnimalAdapter madapter;
 
-public class DishStockFragment extends Fragment {
+    private ArrayList<Animal> animalList;
 
     private TextView mtv;
-    private Button mbt;
-    @Nullable
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dishstock, container, false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        myApplication database = (myApplication)getActivity().getApplicationContext();
+
+        animalList = database.getAnimalList();
+
+        madapter = new AnimalAdapter(animalList,getActivity());
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setListAdapter(madapter);
+        return inflater.inflate(R.layout.fragment_dishstock,null);
     }
 
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mtv = getActivity().findViewById(R.id.faxian);
-        mbt = getActivity().findViewById(R.id.Buttomfaxian);
-        mbt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mtv.setText("点击了dishstock");
-                getActivity().getSupportFragmentManager().beginTransaction().addToBackStack(null).add(R.id.ll_frameLayout,new MenuFragment(),null).commitAllowingStateLoss();
-            }
-        });
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Bundle bundle = new Bundle();
+        bundle.putString("name",animalList.get(position).getName());
+        DishFragment dishFragment = new DishFragment();
+        dishFragment.setArguments(bundle);
+        getActivity().getSupportFragmentManager().beginTransaction().addToBackStack(null).add(R.id.ll_frameLayout,dishFragment,null).commitAllowingStateLoss();
+        Toast.makeText(getActivity(),animalList.get(position).getName(),Toast.LENGTH_SHORT).show();
     }
 }
